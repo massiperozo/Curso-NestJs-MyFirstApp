dcd7256170a7af835e0a9806e6d544f20393731b
Massiel <mpperozo.22@est.ucab.edu.ve>
2025-09-16 15:32:38 -0400

    Creando Servicios desde la terminal y ejecutando la app para visualizar su funcionalidad

---
+++ src/users/users.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'John Doe',
+      phone: '123-456-7890',
+    },
+    {
+      id: 2,
+      name: 'Jane Smith',
+      phone: '987-654-3210',
+    },
+  ];
+
+  getUsers() {
+    return this.users;
+  }
+}
