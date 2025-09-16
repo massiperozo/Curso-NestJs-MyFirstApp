b6ba91ac8a21cf1dfe24d69f31c82185cb4c3c27
Massiel <mpperozo.22@est.ucab.edu.ve>
2025-09-16 15:49:04 -0400

    Entendiendo como funciona el controlador y como se une con el servicio para realizar una peticion http

--- src/tasks/task.controller.ts
+++ src/tasks/task.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
-@Controller({})
+@Controller('/tasks')
 export class TaskController {
   constructor(private tasksService: TasksService) {}
 
-  @Get('/tasks')
+  @Get() // Obtiene todas las tareas
   getAllTasks() {
     return this.tasksService.getTasks();
+  }
+
+  @Post() // Crea una nueva tarea
+  createTask() {
+    return this.tasksService.createTask();
+  }
+
+  @Put() // Actualiza una tarea por completo
+  updateTask() {
+    return this.tasksService.updateTask();
+  }
+
+  @Delete() // Elimina una tarea
+  deleteTask() {
+    return this.tasksService.deleteTask();
+  }
+
+  @Patch() // Actualiza una tarea parcialmente
+  patchTask() {
+    return this.tasksService.patchTask();
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -5,4 +5,20 @@ export class TasksService {
   getTasks(): string[] {
     return ['Task 1', 'Task 2', 'Task 3'];
   }
+
+  createTask(): string {
+    return 'Creando una tarea';
+  }
+
+  updateTask(): string {
+    return 'Actualizando una tarea';
+  }
+
+  deleteTask(): string {
+    return 'Eliminando una tarea';
+  }
+
+  patchTask(): string {
+    return 'Modificando parcialmente una tarea';
+  }
 }
